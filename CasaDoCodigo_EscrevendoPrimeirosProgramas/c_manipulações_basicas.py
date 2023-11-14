f0625b23e07de514941bb9bb07d2155d484a8a99
Bruno Gabriel <34722056+brunogcs@users.noreply.github.com>
2023-11-14 18:13:52 -0300

    Update c_manipulações_basicas.py
    add def, import, date manipultaion, * for unpacking, args and kwargs

--- CasaDoCodigo_EscrevendoPrimeirosProgramas/c_manipulações_basicas.py
+++ CasaDoCodigo_EscrevendoPrimeirosProgramas/c_manipulações_basicas.py
@@ -41,8 +41,6 @@ while True:
     if imposto == 0:
         break
 
-'''
-
 impostos = ['MEI', 'Simples']
 for imposto in impostos:
     print(imposto)
@@ -51,3 +49,66 @@ impostos = ['MEI', 'Simples']
 for x in imposto:
     print(x)
 
+lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+for lista in lista:
+    print(lista)
+
+print('stop')
+
+lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+for i in lista:
+    print(i)
+    
+for i in range(5):
+    print(i)
+    
+    
+impostos = ['MEI', 'Simples']
+for i, imposto in enumerate(impostos):
+    print(i, imposto)
+
+def sum(a, b):
+    return a + b
+c = sum(1, 3)
+print(c)
+
+def salario_descontado_imposto(salario, imposto=27.):
+    return salario - (salario * imposto * 0.01)
+
+res_salario = salario_descontado_imposto(1000)
+print(res_salario)
+
+from datetime import date
+d = (2013, 3, 15)
+print(date(d[0], d[1], d[2]))
+
+from datetime import date
+d = (2013, 3, 15)
+print(date(*d))
+
+def new_user(active=False, admin=False):
+    print(active)
+    print(admin)
+config = {"active": False, "admin": True}
+new_user(**config)
+
+def unpacking_experiment(*args):
+    arg1 = args[0]
+    arg2 = args[1]
+    others = args[2:]
+    print(arg1)
+    print(arg2)
+    print(others)
+unpacking_experiment(1, 2, 3, 4, 5, 6)
+
+'''
+
+def unpacking_experiment(**kwargs):
+    print(kwargs)
+unpacking_experiment(named="Test", other="Other")
+
+def unpacking_experiment2(**kwargs):
+    for key, value in kwargs.items():
+        print(key, value)
+
+unpacking_experiment2(a=1, b=2, c=3)
